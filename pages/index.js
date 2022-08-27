b5868ead89cf556630cbda83eb61dd65f4df5d67
dosorio55 <dosorio55@gmail.com>
2022-08-28 01:44:43 +0200

    added connection with sanity

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { FooterBanner, HeroBanner } from '../components'
+import React from 'react';
+import { FooterBanner, HeroBanner, Product } from '../components';
 
-const Home = () => {
+import { client } from '../lib/client';
+
+const Home = ({products, bannerData}) => {
   return (
     <>
       <HeroBanner />
@@ -11,9 +13,25 @@ const Home = () => {
         <p>Speakers of many variations</p>
       </div>
 
+      <div>
+        {products?.map((product) => product.name)}
+      </div>
       <FooterBanner />
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
 
 export default Home
